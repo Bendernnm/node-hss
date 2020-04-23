2e0997a8e438955bba6dfa3f53813f7cc43abb73
bender <bender.d1mon@gmail.com>
2020-04-23 22:15:34 +0300

    add example - server control

---
+++ __example/server-control.js
@@ -0,0 +1,27 @@
+const { onEvents } = require('./utils');
+
+const { CONSTANTS, StaticServer } = require('../');
+
+const staticServer = StaticServer.setup().startServer();
+
+onEvents(staticServer, CONSTANTS.EVENTS);
+
+// server control
+setTimeout(() => {
+  console.log('timeout1');
+  staticServer.once('serverStop', () => {
+    console.log('timeout2');
+    setTimeout(() => staticServer.startServer(), 15000);
+  });
+
+  staticServer.stopServer();
+}, 5000);
+
+
+setTimeout(() => {
+  console.log('timeout3');
+  staticServer.restartServer();
+}, 50000);
+
+setTimeout(() => {
+}, 100000);
