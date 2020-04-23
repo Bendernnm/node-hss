9a1fabd3d231a3ffe946612fc8f518e8cf7f985c
bender <bender.d1mon@gmail.com>
2020-04-23 22:15:43 +0300

    add example - server already running

---
+++ __example/server-already-running.js
@@ -0,0 +1,14 @@
+const { onEvents } = require('./utils');
+
+const { CONSTANTS, StaticServer } = require('../');
+
+const staticServer = StaticServer.setup({
+  path      : 'public',
+  port      : 4040,
+  host      : '127.0.0.1',
+  setHeaders: { 'X-TEST': 'my personal header' },
+}).startServer();
+
+onEvents(staticServer, CONSTANTS.EVENTS);
+
+staticServer.startServer();
