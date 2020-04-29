4b355d3d11d65d335a29d81882a3f8bfab644947
bender <bender.d1mon@gmail.com>
2020-04-29 20:14:49 +0300

    template - wrong format

---
+++ src/templates/wrong-format.js
@@ -0,0 +1,10 @@
+module.exports = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Wrong file format</title>
+</head>
+<body>
+<h2>Something went wrong on the server side. Wrong format of the file.</h2>
+</body>
+</html>`;
