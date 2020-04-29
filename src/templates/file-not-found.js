824df14eadaad532554466c34cdf6081d4e76d3f
bender <bender.d1mon@gmail.com>
2020-04-29 20:14:39 +0300

    template - file not found

---
+++ src/templates/file-not-found.js
@@ -0,0 +1,10 @@
+module.exports = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>File not found</title>
+</head>
+<body>
+<h2>File not found</h2>
+</body>
+</html>`;
