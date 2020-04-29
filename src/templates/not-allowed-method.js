81aeefea3602748dc85414585ed003ad29687a5e
bender <bender.d1mon@gmail.com>
2020-04-29 20:14:29 +0300

    template - not allowed method

---
+++ src/templates/not-allowed-method.js
@@ -0,0 +1,10 @@
+module.exports = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Not allowed method</title>
+</head>
+<body>
+<h2>Used not allowed method. Allowed methods are GET & HEAD.</h2>
+</body>
+</html>`;
