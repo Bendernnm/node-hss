2784a83f8f4e5507ed21c029151325e12b9ba483
bender <bender.d1mon@gmail.com>
2020-04-29 20:15:25 +0300

    templates

---
+++ src/templates/index.js
@@ -0,0 +1,9 @@
+const fileNotFound = require('./file-not-found');
+const wrongFileFormat = require('./wrong-format');
+const notAllowedMethod = require('./not-allowed-method');
+
+module.exports = {
+  fileNotFound,
+  wrongFileFormat,
+  notAllowedMethod,
+};
