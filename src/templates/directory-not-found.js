1439c6adbd2e015e560f8252ce25f1b6182f70f1
bender <bender.d1mon@gmail.com>
2020-04-30 21:03:41 +0300

    template - directory not found

---
+++ src/templates/directory-not-found.js
@@ -0,0 +1,10 @@
+module.exports = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Directory not found</title>
+</head>
+<body>
+<h2>Directory not found</h2>
+</body>
+</html>`;
