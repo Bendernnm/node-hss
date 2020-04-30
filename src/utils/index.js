244e42913ca18c2a275f75fd8d13aedae24203d9
bender <bender.d1mon@gmail.com>
2020-04-30 21:04:03 +0300

    connect utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,9 @@
+const getFileInfo = require('./get-file-info');
 const errorResponse = require('./error-response');
+const buildDirectoryStructure = require('./build-directories-structure');
 
-module.exports = { errorResponse };
+module.exports = {
+  getFileInfo,
+  errorResponse,
+  buildDirectoryStructure,
+};
