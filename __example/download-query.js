2dcdd9b32553b33f86e118cd2316641c2fc6b3c9
bender <bender.d1mon@gmail.com>
2020-04-30 21:17:43 +0300

    change download query example

--- __example/download-query.js
+++ __example/download-query.js
@@ -6,8 +6,8 @@ const staticServer = StaticServer.setup({
   path             : 'public',
   port             : 4040,
   host             : '127.0.0.1',
-  setHeaders       : { 'X-TEST': 'my personal header' },
   downloadFileQuery: 'download',
+  downloadFileName : (fileName) => `${fileName.split('/').pop()}`,
 }).startServer();
 
 onEvents(staticServer, CONSTANTS.EVENTS);
