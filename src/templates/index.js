bfcf7133ed42fd7097a9359cf8ca7f46336b037a
bender <bender.d1mon@gmail.com>
2020-04-30 21:03:56 +0300

    connect template

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,9 +1,11 @@
 const fileNotFound = require('./file-not-found');
 const wrongFileFormat = require('./wrong-format');
 const notAllowedMethod = require('./not-allowed-method');
+const directoryNotFound = require('./directory-not-found');
 
 module.exports = {
   fileNotFound,
   wrongFileFormat,
   notAllowedMethod,
+  directoryNotFound,
 };
