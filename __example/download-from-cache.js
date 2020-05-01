8de12bbceac075feaa98da0e0aa1f8447d100e40
bender <bender.d1mon@gmail.com>
2020-05-01 17:44:36 +0300

    example - fix issue

--- __example/download-from-cache.js
+++ __example/download-from-cache.js
@@ -3,15 +3,15 @@ const { onEvents } = require('./utils');
 const { CONSTANTS, StaticServer } = require('../');
 
 const staticServer = StaticServer.setup({
-  path            : 'public',
-  port            : 4040,
-  host            : '127.0.0.1',
-  setHeaders      : { 'X-TEST': 'my personal header' },
-  downloadFile    : true,
-  downloadFileName: (fileName) => `${fileName.split('/').pop()}`,
-  useCache  : 1,
-  cacheOpts : {
-    maxSizeOfCache: 1024, // required, bytes
+  path             : 'public',
+  port             : 4040,
+  host             : '127.0.0.1',
+  setHeaders       : { 'X-TEST': 'my personal header' },
+  downloadFileQuery: 'download',
+  downloadFileName : (fileName) => `${fileName.split('/').pop()}`,
+  useCache         : 1,
+  cacheOpts        : {
+    maxSizeOfCache     : 1024, // required, bytes
     maxSizeOfCachedFile: 1024, // required, bytes
   },
 }).startServer();
