85a90b87df0a8e47ca3deeac3b1194f02eb11eb4
bender <bender.d1mon@gmail.com>
2020-05-03 02:36:38 +0300

    file watcher change logic of handler

--- src/cache/file-watcher.js
+++ src/cache/file-watcher.js
@@ -28,7 +28,7 @@ class FileWatcher {
     this.watcher.close();
   }
 
-  async watcherHandler(eventType, filePath) {
+  async watcherHandler(eventType, fileName) {
     if (eventType === FileWatcher.constants.ET_CLOSE) {
       return this.watcher = null;
     }
@@ -37,33 +37,24 @@ class FileWatcher {
       return this.stopWatch();
     }
 
-    if (eventType === FileWatcher.constants.ET_RENAME) {
-      const newFilePath = this.filePath.replace(this.fileOriginName, filePath);
+    if (eventType === FileWatcher.constants.ET_CHANGE) {
+      return FileWatcher.fileWatcherEvents.emit(FileWatcher.constants.E_EDITED,
+        { filePath: this.filePath });
+    }
+
+    // eventType === 'rename'
+    const newFilePath = this.filePath.replace(this.fileOriginName, fileName);
+
+    try {
+      await fs.promises.access(newFilePath, fs.constants.R_OK);
 
       FileWatcher.fileWatcherEvents.emit(FileWatcher.constants.E_RENAMED, {
         newFilePath,
         filePath: this.filePath,
-      });
-
-      return this.filePath = filePath;
-    }
-
-    // eventType === 'change'
-    // if (filePath !== this.filePath) { // renamed
-    //   FileWatcher.fileWatcherEvents.emit(FileWatcher.constants.E_RENAMED, {
-    //     newFilePath: filePath,
-    //     filePath   : this.filePath,
-    //   });
-    //
-    //   return this.filePath = filePath;
-    // }
-
-    try {
-      await fs.promises.access(filePath, fs.constants.R_OK);
-
-      FileWatcher.fileWatcherEvents.emit(FileWatcher.constants.E_EDITED, { filePath }); // edited
+      }); // rename
     } catch (err) {
-      FileWatcher.fileWatcherEvents.emit(FileWatcher.constants.E_DELETED, { filePath }); // deleted
+      FileWatcher.fileWatcherEvents.emit(FileWatcher.constants.E_DELETED,
+        { filePath: this.filePath }); // deleted
     }
   }
 
