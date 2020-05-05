aeec7422d9bdf5cbed1c4206ed1662da4cdf8236
bender <bender.d1mon@gmail.com>
2020-05-06 00:04:57 +0300

    types/utils

---
+++ @types/src/utils/index.d.ts
@@ -0,0 +1,11 @@
+import getFileInfo from './get-file-info';
+import errorResponse from './error-response';
+import ErrorMessages from './error-messages';
+import buildDirectoryStructure from './build-directories-structure';
+
+export = {
+    getFileInfo,
+    errorResponse,
+    ErrorMessages,
+    buildDirectoryStructure,
+};
