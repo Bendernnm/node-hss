d1147f6c3625dbd329833d880dc4520df90d4a94
bender <bender.d1mon@gmail.com>
2020-05-05 19:05:34 +0300

    new util - error message

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,10 @@
 const getFileInfo = require('./get-file-info');
+const ErrorMessages = require('./error-messages');
 const errorResponse = require('./error-response');
 const buildDirectoryStructure = require('./build-directories-structure');
 
 module.exports = {
+  ErrorMessages,
   getFileInfo,
   errorResponse,
   buildDirectoryStructure,
