aed306af178726cca5fa65ec23b279152c11970f
bender <bender.d1mon@gmail.com>
2020-05-07 17:37:46 +0300

    errors - module

---
+++ src/error/index.js
@@ -0,0 +1,11 @@
+const Errors = require('./errors');
+const CustomError = require('./custom-error');
+const ERROR_CODES = require('./error-codes');
+const ERROR_MESSAGES = require('./error-messages');
+
+module.exports = {
+  Errors,
+  CustomError,
+  ERROR_CODES,
+  ERROR_MESSAGES,
+};
