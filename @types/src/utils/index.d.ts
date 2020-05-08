67409ccb53a7aa88d7a1a07fcef98208dc1112eb
bender <bender.d1mon@gmail.com>
2020-05-08 22:06:34 +0300

    types/utils

--- @types/src/utils/index.d.ts
+++ @types/src/utils/index.d.ts
@@ -1,7 +1,11 @@
+import pathIsSafe from './path-is-safe';
 import getFileInfo from './get-file-info';
+import verifyRequestMethod from './verify-request-method';
 import buildDirectoryStructure from './build-directories-structure';
 
 export = {
+    pathIsSafe,
     getFileInfo,
+    verifyRequestMethod,
     buildDirectoryStructure,
 };
