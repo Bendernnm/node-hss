4052ff4475890454366c38a318af43e86490a10b
bender <bender.d1mon@gmail.com>
2020-05-08 22:27:33 +0300

    utils connect pipeStreams

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,12 @@
 const pathIsSafe = require('./path-is-safe');
+const pipeStreams = require('./pipe-streams');
 const getFileInfo = require('./get-file-info');
 const verifyRequestMethod = require('./verify-request-method');
 const buildDirectoryStructure = require('./build-directories-structure');
 
 module.exports = {
   pathIsSafe,
+  pipeStreams,
   getFileInfo,
   verifyRequestMethod,
   buildDirectoryStructure,
