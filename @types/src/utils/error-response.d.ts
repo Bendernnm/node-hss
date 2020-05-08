db860439fca6217f500c607de11dc08ab265394e
bender <bender.d1mon@gmail.com>
2020-05-08 22:02:09 +0300

    types - remove deprecated utils

--- @types/src/utils/error-response.d.ts
+++
@@ -1,9 +0,0 @@
-interface Parameters {
-    msg: string;
-    headers: any;
-    statusCode: number;
-}
-
-declare function errorResponse(parameters: Parameters, end: boolean = true);
-
-export = errorResponse;
--- @types/src/utils/index.d.ts
+++ @types/src/utils/index.d.ts
@@ -1,11 +1,7 @@
 import getFileInfo from './get-file-info';
-import errorResponse from './error-response';
-import ErrorMessages from './error-messages';
 import buildDirectoryStructure from './build-directories-structure';
 
 export = {
     getFileInfo,
-    errorResponse,
-    ErrorMessages,
     buildDirectoryStructure,
 };
