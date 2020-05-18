8499ec4a86b86f87742793f9e5fc89dea2ea368e
bender <bender.d1mon@gmail.com>
2020-05-18 14:33:18 +0300

    tests/unit/utils/get-file-info - init & describe Custom file

---
+++ tests/unit/utils/get-file-info.test.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+const mime = require('mime');
+
+const { getFileInfo } = require('../../../src/utils');
+
+function filesBeforeAllMocks({ staticPath, url, fileExtension, fileMimeType }) {
+  jest.clearAllMocks();
+
+  jest.spyOn(path, 'extname').mockReturnValue(`.${fileExtension}`);
+  jest.spyOn(path, 'join').mockReturnValue(`${staticPath}${url}`);
+
+  jest.spyOn(mime, 'getType').mockReturnValue(fileMimeType);
+}
+
+describe('Custom files', () => {
+  const url = '/public/1.txt';
+  const staticPath = '/static';
+
+  const fileExtension = 'txt';
+  const fileMimeType = 'text/plain';
+  const filePath = `${staticPath}${url}`;
+
+  beforeAll(() => filesBeforeAllMocks({
+    url,
+    staticPath,
+    fileMimeType,
+    fileExtension,
+  }));
+
+  it('should return file info, useDefaultFileName = true', () => {
+    const fileInfo = getFileInfo(staticPath, url, true);
+
+    expect(fileInfo).toEqual({
+      filePath,
+      fileMimeType,
+      fileExtension,
+      fileName      : url,
+      fileOriginName: '1.txt',
+    });
+  });
+
+  it('should return file info, useDefaultFileName = false', () => {
+    const fileInfo = getFileInfo(staticPath, url, false);
+
+    expect(fileInfo).toEqual({
+      filePath,
+      fileMimeType,
+      fileExtension,
+      fileName      : url,
+      fileOriginName: '1.txt',
+    });
+  });
+
+  afterAll(() => jest.clearAllMocks());
+});
